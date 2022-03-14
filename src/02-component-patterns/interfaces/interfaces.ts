bbe38e917e4fbd4feee4962c52d4ca29f6c1196e
Marcos Ruales <markosrua1994@gmail.com>
2022-03-14 18:24:24 -0500

    Fin del patron

--- src/02-component-patterns/interfaces/interfaces.ts
+++ src/02-component-patterns/interfaces/interfaces.ts
@@ -14,6 +14,7 @@ export interface ProductContextProps {
     counter: number;
     product: Product;
     increaseBy: ( value: number ) => void;
+    maxCount?: number
 }
 
 
@@ -33,4 +34,19 @@ export interface onChangeArgs {
 
 export interface ProductInCart extends Product {
     count: number
+}
+
+export interface InitialValues {
+    count?: number,
+    maxCount?: number
+}
+
+export interface ProductCardHandlers {
+    count: number,
+    isMaxCountReached: boolean,
+    maxCount?: number,
+    product: Product
+
+    increaseBy: ( value: number ) => void,
+    reset: () => void
 }
---
+++ src/pages/LazyPage3.tsx
@@ -0,0 +1,9 @@
+
+export const LazyPage3 = () => {
+    return (
+        <div>
+            <h1>Lazy pAGE 3</h1>
+        </div>
+    )
+}
+//export default LazyPage3
